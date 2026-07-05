fa5366940ce0f80df4565960074e7ff0224f7b7e
game <shyamraj.p.r777@gmail.com>
2026-07-05 12:18:56 +0530

    Add dreamscapePrompt and imageBase64 fields to DiaryEntry model

--- lib/models/diary_entry.dart
+++ lib/models/diary_entry.dart
@@ -4,6 +4,8 @@ class DiaryEntry {
   final String summary;
   final List<String> keyInsights;
   final List<String> conversationPreviews;
+  final String? dreamscapePrompt;
+  final String? imageBase64;
 
   const DiaryEntry({
     required this.id,
@@ -11,6 +13,8 @@ class DiaryEntry {
     required this.summary,
     this.keyInsights = const [],
     this.conversationPreviews = const [],
+    this.dreamscapePrompt,
+    this.imageBase64,
   });
 
   Map<String, dynamic> toJson() => {
@@ -19,6 +23,8 @@ class DiaryEntry {
     'summary': summary,
     'keyInsights': keyInsights,
     'conversationPreviews': conversationPreviews,
+    if (dreamscapePrompt != null) 'dreamscapePrompt': dreamscapePrompt,
+    if (imageBase64 != null) 'imageBase64': imageBase64,
   };
 
   factory DiaryEntry.fromJson(Map<String, dynamic> json) => DiaryEntry(
@@ -27,5 +33,7 @@ class DiaryEntry {
     summary: json['summary'] as String,
     keyInsights: List<String>.from(json['keyInsights'] as List),
     conversationPreviews: List<String>.from(json['conversationPreviews'] as List),
+    dreamscapePrompt: json['dreamscapePrompt'] as String?,
+    imageBase64: json['imageBase64'] as String?,
   );
 }
